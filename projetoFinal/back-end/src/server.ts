49f960958a131df6fac69b7655ceaf016436757f
smrenato <renatosmx@gmail.com>
2022-06-27 07:21:03 -0300

    add: api-docs

--- projetoFinal/back-end/src/server.ts
+++ projetoFinal/back-end/src/server.ts
@@ -9,10 +9,16 @@ import routes from "./routes";
 
 import "./database";
 
+import swaggerUI from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(routes);
 
