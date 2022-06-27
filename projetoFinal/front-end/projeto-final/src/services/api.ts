13b093d81ced3a22c89df2a32f8379092a25db83
smrenato <renatosmx@gmail.com>
2022-06-27 14:13:00 -0300

    add: axiosApi base url

--- projetoFinal/front-end/projeto-final/src/services/api.ts
+++ projetoFinal/front-end/projeto-final/src/services/api.ts
@@ -1,8 +1,7 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+    baseURL: "https://app-projeto-final-backend.herokuapp.com",
 });
 
 export default api;
